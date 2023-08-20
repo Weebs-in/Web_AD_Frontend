d85123711b99ebb8ee2c3785e93c605af6fe467e
ruthtop <mud.cookie@gmail.com>
2023-08-20 12:01:22 +0800

    added different homepage landing for different user type

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -1,17 +1,28 @@
-// material-ui
-import { Typography } from '@mui/material';
+// // material-ui
+// import { Typography } from '@mui/material';
+//
+// // project imports
+// import MainCard from 'ui-component/cards/MainCard';
 
-// project imports
-import MainCard from 'ui-component/cards/MainCard';
+import { getUserRoleFromLS } from '../../utils/jwtUtils';
+import config from '../../config';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const Home = () => {
-  return (
-    <MainCard title="Welcome">
-      <Typography variant="body2">Please click on the sidebar to get started with your tasks</Typography>
-    </MainCard>
-  );
+  const role = getUserRoleFromLS();
+  if (role === config.USER_ROLE_ADMIN) {
+    window.location.href = '/dashboard/default';
+  } else if (role === config.USER_ROLE_MODERATOR) {
+    window.location.href = '/moderator/collection-view';
+  } else {
+    window.location.href = '/login';
+  }
+  // return (
+  //   <MainCard title="Welcome">
+  //     <Typography variant="body2">Please click on the sidebar to get started with your tasks</Typography>
+  //   </MainCard>
+  // );
 };
 
 export default Home;
